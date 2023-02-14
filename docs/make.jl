8dc8ce88799d61f8f280c0eaf05469a044d41f38
obertscheiderfhwn <christof.obertscheider@fhwn.ac.at>
2023-02-14 20:50:11 +0100

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -5,3 +5,7 @@ makedocs(sitename="RTMsim documentation",
          modules=[rtmsim],
          pages=["Home" => "index.md"]
         )
+
+deploydocs(
+    repo = "github.com/obertscheiderfhwn/RTMsim.git",
+)
