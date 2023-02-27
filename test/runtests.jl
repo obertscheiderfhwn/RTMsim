2aba44ba5001e196c17372461b1247be069280f2
obertscheiderfhwn <christof.obertscheider@fhwn.ac.at>
2023-02-27 20:58:54 +0100

    tests failed

--- test/runtests.jl
+++ test/runtests.jl
@@ -10,7 +10,8 @@ println("...for running a simulation")
 MODULE_ROOT=splitdir(splitdir(pathof(rtmsim))[1])[1]
 meshfilename=joinpath(MODULE_ROOT,"meshfiles","mesh_permeameter1_foursets.bdf")
 #print(meshfilename*" \n")
-rtmsim.rtmsim_rev1(1,meshfilename,200, 101325,1.225,1.4,0.06, 1.35e5,1.00e5, 3e-3,0.7,3e-10,1,1,0,0, 3e-3,0.7,3e-10,1,1,0,0, 3e-3,0.7,3e-11,1,1,0,0, 3e-3,0.7,3e-11,1,1,0,0, 3e-3,0.7,3e-9,1,1,0,0, 1,2,2,2,0,"results.jld2",0,0.01,16)
+param=rtmsim.input_vals(1,meshfilename,200, 101325,1.225,1.4,0.06, 1.35e5,1.00e5, 3e-3,0.7,3e-10,1,1,0,0, 3e-3,0.7,3e-10,1,1,0,0, 3e-3,0.7,3e-11,1,1,0,0, 3e-3,0.7,3e-11,1,1,0,0, 3e-3,0.7,3e-9,1,1,0,0, 1,2,2,2,0,"results.jld2",0,0.01,16);
+rtmsim.rtmsim_rev1(param)
 
 println("...for plotting the results")
 WORK_DIR=pwd();
